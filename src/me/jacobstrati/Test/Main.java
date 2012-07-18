4b7320c9c37624b6492bfaebdf799f3463e704f9
Amir Ali Omidi <aa_omidi@yahoo.com>
2012-07-18 15:44:23 -0400

    Added def

--- src/me/jacobstrati/Test/Main.java
+++ src/me/jacobstrati/Test/Main.java
@@ -1,8 +1,12 @@
 package me.jacobstrati.Test;
 
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+
 public class Main {
     
     public static final Logger log = Bukkit.getLogger();
+    public static Main plugin;
     
 
 }
